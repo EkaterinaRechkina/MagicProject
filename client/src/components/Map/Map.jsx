ff5e7c5f2b3b8134ded755bfae6acb5a7f3cf535
EkaterinaRechkina <ekaterinarechkinav@yandex.ru>
2022-07-14 22:42:31 +0300

    change colors

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -39,13 +39,19 @@ export default function Map() {
 
     setMap(map);
 
+    const customPopup =
+      '<p style="display:inline">' +
+      '<img src="https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:10px"><span style="font:14px Gotham">You are here</span></br>' +
+      '<div style="font:9px Gotham"><span style="color:grey">Best place to be in</span><div><br/>' +
+      '<span style="font:10px Gotham">London,<br/>UK</span></br>' +
+      "</div></p>";
+
     const addMarker = () => {
       const popupOffset = {
         bottom: [0, -25],
       };
-      const popup = new tt.Popup({ offset: popupOffset }).setHTML(
-        "This this you"
-      );
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML(customPopup);
       const element = document.createElement("div");
       element.className = "marker";
 
@@ -76,58 +82,144 @@ export default function Map() {
       place.setPopup(popup).togglePopup();
     };
 
+    const newOrleanPopup =
+      '<p style="display:inline">' +
+      '<img src="https://images.unsplash.com/photo-1616463539061-adcd0d7664de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:10px"><span style="font:14px Gotham">French quarter voodoo</span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/New_Orleans" target="_blank">New Orlean,USA </a>' +
+      "</div></p>";
+
     const markerNewOrlean = new tt.Marker()
       .setLngLat([-90.071533, 29.951065])
       .addTo(map);
 
-    createPopup("New Orlean, USA", markerNewOrlean);
+    createPopup(newOrleanPopup, markerNewOrlean);
+
+    const stonehengePopup =
+      '<p style="display:inline">' +
+      '<img src="https://images.unsplash.com/photo-1519526933243-dd9747bfaae5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:10px"><span style="font:14px Gotham">Stonehenge is a prehistoric monument</span></br>' +
+      '<a href="https://www.english-heritage.org.uk/visit/places/stonehenge/" target="_blank">Salisbury Plain in Wiltshire,UK </a>' +
+      "</div></p>";
 
     const markerStonehenge = new tt.Marker()
       .setLngLat([-1.826215, 51.178882])
       .addTo(map);
 
     createPopup(
-      "Stonehenge, UK",
+      stonehengePopup,
 
       markerStonehenge
     );
 
+    const catemacoPopup =
+      '<p style="display:inline">' +
+      '<img src="https://images.unsplash.com/photo-1518105779142-d975f22f1b0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:10px"><span style="font:14px Gotham">Catemaco is known in Mexico for its community of “brujos", which can be translated as "witches" or "sorcerers"</span></br>' +
+      '<a href="https://fr.wikipedia.org/wiki/Catemaco" target="_blank">Catemaco,Mexico </a>' +
+      "</div></p>";
+
     const markerCatemaco = new tt.Marker()
       .setLngLat([-95.11398, 18.42131])
       .addTo(map);
-    createPopup("Catemaco, Mexico", markerCatemaco);
+    createPopup(catemacoPopup, markerCatemaco);
+
+    const salemPopup =
+      '<p style="display:inline">' +
+      '<img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/67/ae/97.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:10px"><span style="font:10px Gotham">This charming and typical Massachusetts village went down in history because of the witch hunts that took place in 1692</span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Salem,_Massachusetts" target="_blank">Salem, Massachusetts</a>' +
+      "</div></p>";
+
     const markerSalem = new tt.Marker()
       .setLngLat([-70.896713, 42.519539])
       .addTo(map);
-    createPopup("Salem, USA", markerSalem);
+    createPopup(salemPopup, markerSalem);
+
+    const trioraPopup =
+      '<p style="display:inline">' +
+      '<img src="https://thumbs.dreamstime.com/b/street-triora-4-4886510.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">This village is often called “the Italian Salem”. </span></br>' +
+      '<a href="https://fr.wikipedia.org/wiki/Triora" target="_blank">Triora, Liguria</a>' +
+      "</div></p>";
+
     const markerTriora = new tt.Marker()
       .setLngLat([7.7653, 43.9931])
       .addTo(map);
-    createPopup("Triora, Liguria", markerTriora);
+    createPopup(trioraPopup, markerTriora);
+
+    const turinPopup =
+      '<p style="display:inline">' +
+      '<img src="https://cdn.pixabay.com/photo/2016/10/31/14/53/italy-1785754_960_720.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">Beautiful Turin, joined by Lyon and Prague, is one of the three cities on the “white magic axis” ”. </span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Turin" target="_blank">Turin, Piedmont</a>' +
+      "</div></p>";
+
     const markerTurin = new tt.Marker()
       .setLngLat([7.6868565, 45.070312])
       .addTo(map);
-    createPopup("Turin, Piedmont", markerTurin);
+    createPopup(turinPopup, markerTurin);
+
+    const holmavikPopup =
+      '<p style="display:inline">' +
+      '<img src="https://thumbs.dreamstime.com/z/panorama-holmavik-town-iceland-church-harbour-panorama-holmavik-town-iceland-church-pier-harbour-199557673.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px;padding-right:14px"><span style="font:10px Gotham">In Iceland"s Holmavik, pagan culture and Christianity coexisted until at least 1000 AD ”. </span></br>' +
+      '<a href="https://www.westfjords.is/en/destinations/towns/holmavik" target="_blank"> Holmavik, Iceland </a>' +
+      "</div></p>";
+
     const markerHolmavik = new tt.Marker()
       .setLngLat([-21.6845875, 65.7021287])
       .addTo(map);
-    createPopup("Holmavik, Iceland", markerHolmavik);
+    createPopup(holmavikPopup, markerHolmavik);
+
+    const togoPopup =
+      '<p style="display:inline">' +
+      '<img src="https://image.shutterstock.com/image-photo/voodoo-objects-use-rituals-spells-600w-1193607403.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">In Togoville in West Africa, voodoo and the magical arts are part of everyday life ”. </span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Togoville" target="_blank">Togoville, Togo</a>' +
+      "</div></p>";
+
     const markerTogo = new tt.Marker()
       .setLngLat([1.478102, 6.233659])
       .addTo(map);
-    createPopup("Togoville, Togo", markerTogo);
+    createPopup(togoPopup, markerTogo);
+
+    const hookPopup =
+      '<p style="display:inline">' +
+      '<img src="https://image.shutterstock.com/image-photo/aerial-view-hook-lighthouse-building-600w-1495821716.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">In Ireland, one can find very many places that have local legends with deep magical and esoterical meaning. ”. </span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Hook_Peninsula" target="_blank">The Hook Peninsula, Ireland</a>' +
+      "</div></p>";
+
     const markerHook = new tt.Marker()
       .setLngLat([-6.920556, 52.121389])
       .addTo(map);
-    createPopup("The Hook Peninsula, Ireland", markerHook);
+    createPopup(hookPopup, markerHook);
+
+    const phnomPopup =
+      '<p style="display:inline">' +
+      '<img src="https://image.shutterstock.com/image-photo/phnom-penh-riverside-sunrise-cambodia-600w-1277484061.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">Temple of Wat Phnom will cure you of the evil eye by putting pieces of meat and fruit in the mouths of the dragon statues as offerings ”. </span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Phnom_Penh" target="_blank">Phnom Penh, Cambodia</a>' +
+      "</div></p>";
+
     const markerPhnom = new tt.Marker()
       .setLngLat([104.888535, 11.562108])
       .addTo(map);
-    createPopup("Phnom Penh, Cambodia", markerPhnom);
+    createPopup(phnomPopup, markerPhnom);
+
+    const altayPopup =
+      '<p style="display:inline">' +
+      '<img src="https://www.altaiproject.org/wp-content/uploads/2020/12/Chagat_IMG_6779-1024x683.jpg" alt="pizza oven" style="width:50%;float:right;padding-top:10px">' +
+      '<div style="width:50%; height:100%; padding-top:14px"><span style="font:10px Gotham">Shamanic rituals, connection with power places and nature. ”. </span></br>' +
+      '<a href="https://en.wikipedia.org/wiki/Altai_Republic" target="_blank">Altai, Russia</a>' +
+      "</div></p>";
+
     const markerAltay = new tt.Marker()
       .setLngLat([79.782372, 51.696396])
       .addTo(map);
-    createPopup("Altai, Russia", markerAltay);
+    createPopup(altayPopup, markerAltay);
+
     return () => map.remove();
   }, []);
 
--- client/src/components/OneUserProduct/OneUserProduct.jsx
+++ client/src/components/OneUserProduct/OneUserProduct.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -6,165 +6,168 @@ import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { editProduct, deleteProduct } from "../../redux/actions/product.action";
 import Popover from "@mui/material/Popover";
-import PopupState, { bindPopover } from "material-ui-popup-state";
-import {Box, Modal, TextField} from "@mui/material";
+import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state";
+import { Box, Modal, TextField } from "@mui/material";
+import "../Story/Story";
 
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-    display: "flex",
-    flexDirection: "column",
-    gap: "20px",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
 };
 
 function OneUserProduct({ id, author, title, description, img, price }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const [newImg, setNewImg] = useState(img);
+  const [newPrice, setNewPrice] = useState(price);
 
-    const [newTitle, setNewTitle] = useState(title);
-    const [newDescription, setNewDescription] = useState(description);
-    const [newImg, setNewImg] = useState(img);
-    const [newPrice, setNewPrice] = useState(price);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  function deleteProductHandler(id) {
+    dispatch(deleteProduct(id));
+  }
 
-    function deleteProductHandler(id) {
-        dispatch(deleteProduct(id));
-    }
+  function editHandler(id, title, description, img, price) {
+    dispatch(editProduct(id, title, description, img, price));
+    handleClose();
+  }
 
-    function editHandler(id, title, description, img, price) {
-        dispatch(editProduct(id, title, description, img, price));
-        handleClose();
-    }
+  return (
+    <div className="story">
+      <PopupState variant="popover" popupId="demo-popup-popover">
+        {(popupState) => (
+          <div id={id}>
+            <Card sx={{ maxWidth: 205, position: "relative", margin: 5 }}>
+              <CardMedia
+                component="img"
+                height="200"
+                image={img}
+                alt=""
+                {...bindTrigger(popupState)}
+              />
 
-    return (
-        <>
-            <PopupState variant="popover" popupId="demo-popup-popover">
-                {(popupState) => (
-                    <div id={id}>
-                        <Card sx={{ maxWidth: 205, position: "relative", margin: 5}}>
-                            <CardMedia component="img" height="200" image={img} alt=""/>
+              <CardContent sx={{ height: 120 }}>
+                <Button id={id} sx={{ position: "absolute", top: 5, right: 0 }}>
+                  <DeleteIcon onClick={() => deleteProductHandler(id)} />
+                </Button>
+                <Button
+                  id={id}
+                  sx={{ position: "absolute", top: 5, right: 35 }}
+                >
+                  <EditIcon onClick={handleOpen} />
+                </Button>
+                <div className="title">{title}</div>
+                <div>Price: {price}</div>
+                <div>Author: {author}</div>
+              </CardContent>
+            </Card>
 
-                            <CardContent sx={{ height: 90 }}>
-                                <Button id={id} sx={{ position: "absolute", top: 5, right: 0 }}>
-                                    <DeleteIcon onClick={() => deleteProductHandler(id)} />
-                                </Button>
-                                <Button id={id} sx={{ position: "absolute", top: 5, right: 35 }}>
-                                    <EditIcon onClick={handleOpen} />
-                                </Button>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {title}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {description}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Price: {price}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Author: {author}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-
-                        <Popover
-                            {...bindPopover(popupState)}
-                            anchorOrigin={{
-                                vertical: "center",
-                                horizontal: "center",
-                            }}
-                            transformOrigin={{
-                                vertical: "center",
-                                horizontal: "center",
-                            }}
-                        >
-                            <Typography sx={{ width: "650px", height: "600px", padding: "40px" }}>
-                                <CardMedia
-                                    component="img"
-                                    height="350"
-                                    image={img}
-                                    alt={title}
-                                />
-                                <br />
-                                <br />
-                                {title}
-                                <br />
-                                <br />
-                                <br />
-                                {description}
-                            </Typography>
-                        </Popover>
-                    </div>
-                )}
-            </PopupState>
-
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
+            <Popover
+              {...bindPopover(popupState)}
+              anchorOrigin={{
+                vertical: "center",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "center",
+                horizontal: "center",
+              }}
             >
-                <Box sx={style}>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Image"
-                        value={newImg}
-                        onChange={(event) => setNewImg(event.target.value)}
-                        required
-                    />
-                    <TextField
-                        onChange={(event) => setNewTitle(event.target.value)}
-                        value={newTitle}
-                        id="standard-basic"
-                        label="Title"
-                        variant="standard"
-                        required
-                    />
-                    <TextField
-                        onChange={(event) => setNewDescription(event.target.value)}
-                        value={newDescription}
-                        id="standard-basic"
-                        label="Description"
-                        variant="standard"
-                        required
-                    />
-                    <TextField
-                        onChange={(event) => setNewPrice(event.target.value)}
-                        value={newPrice}
-                        id="standard-basic"
-                        label="Price"
-                        variant="standard"
-                        required
-                    />
-                    <Button
-                        id={id}
-                        size="small"
-                        onClick={() => editHandler(id, newTitle, newDescription, newImg, newPrice)}
-                    >
-                        Submit
-                    </Button>
-                    <Button size="small" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Box>
-            </Modal>
+              <div className="popup">
+                <CardMedia
+                  component="img"
+                  height="350"
+                  image={img}
+                  alt={title}
+                />
+                <div className="title-popup"> {title}</div>
+                <div className="description-popup ">{description}</div>
+              </div>
+            </Popover>
+          </div>
+        )}
+      </PopupState>
 
-        </>
-
-    );
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField
+            required
+            id="outlined-required"
+            label="Image"
+            value={newImg}
+            onChange={(event) => setNewImg(event.target.value)}
+          />
+          <TextField
+            onChange={(event) => setNewTitle(event.target.value)}
+            value={newTitle}
+            id="standard-basic"
+            label="Title"
+            variant="standard"
+            required
+          />
+          <TextField
+            onChange={(event) => setNewDescription(event.target.value)}
+            value={newDescription}
+            id="standard-basic"
+            label="Description"
+            variant="standard"
+            required
+          />
+          <TextField
+            onChange={(event) => setNewPrice(event.target.value)}
+            value={newPrice}
+            id="standard-basic"
+            label="Price"
+            variant="standard"
+            required
+          />
+          <Button
+            id={id}
+            sx={{
+              color: "#2b256f",
+            }}
+            size="small"
+            onClick={() =>
+              editHandler(id, newTitle, newDescription, newImg, newPrice)
+            }
+          >
+            Submit
+          </Button>
+          <Button
+            size="small"
+            sx={{
+              color: "#2b256f",
+            }}
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
 export default OneUserProduct;
