be151f8b8f88c5644c61900890a188bdd1733fdf
EkaterinaRechkina <ekaterinarechkinav@yandex.ru>
2022-07-19 12:10:01 +0300

    0.2

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -3,7 +3,7 @@ import * as tt from "@tomtom-international/web-sdk-maps";
 import * as ttapi from "@tomtom-international/web-sdk-services";
 import SearchBox from "@tomtom-international/web-sdk-plugin-searchbox";
 import "./map.css";
-import 'animate.css';
+import "animate.css";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
 export default function Map() {
@@ -396,9 +396,17 @@ export default function Map() {
         </p>
       </div>
       <div ref={mapElement} className="map"></div>
-      <div id='cat' className="cat">
-        <img src={require('../../images/cat_sitting.gif')} alt='#' className='catGifSitting animate__animated animate__bounceIn animate__delay-5s'/>
-        <img src={require('../../images/black_cat.gif')} alt='#' className='catGif'/>
+      <div id="cat" className="cat">
+        <img
+          src={require("../../images/cat_sitting.gif")}
+          alt="#"
+          className="catGifSitting animate__animated animate__bounceIn animate__delay-3s"
+        />
+        <img
+          src={require("../../images/black_cat.gif")}
+          alt="#"
+          className="catGif"
+        />
       </div>
     </div>
   );
