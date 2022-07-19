e07283d687e714a6d24e84e00284d82eac003fdd
a7heis7 <a7295@yandex.ru>
2022-07-19 14:45:01 +0300

    cartfixes

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./style.module.css";
 import { createOrder, removeFromCart } from "../../redux/actions/cart.actions";
+import CartElement from "./CartElement";
 
 function Cart() {
   const cart = useSelector((store) => store.cart);
-  const dispatch = useDispatch();
   const userId = useSelector((store) => store.user[0]);
   const [order, setOrder] = useState(false);
 
@@ -19,20 +19,7 @@ function Cart() {
       <div className={style.cart}>
         <ul className={style.list}>
           {cart.length ? (
-            cart.map((el) => (
-              <>
-                <li className={style.listItem} key={el.id}>
-                  <div className={style.imgBlock}>
-                    <img className={style.image} src={el.img} alt="" />
-                  </div>
-                  <div className={style.descr}>{el.description}</div>
-                  {el.price}
-                  <Button onClick={() => dispatch(removeFromCart(el))}>
-                    X
-                  </Button>
-                </li>
-              </>
-            ))
+            cart.map((item) => <CartElement {...item} key={item.id}/>)
           ) : (
             <p>Your cart still empty</p>
           )}
